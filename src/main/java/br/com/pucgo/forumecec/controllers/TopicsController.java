8b1aefa20f5b18e0654ce3252af8a5b515e5e162
apontes77 <alexandrepontesgyn@gmail.com>
2021-08-24 06:46:28 -0300

    simplificando a paginação e ordenação

--- src/main/java/br/com/pucgo/forumecec/controllers/TopicsController.java
+++ src/main/java/br/com/pucgo/forumecec/controllers/TopicsController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -36,11 +37,8 @@ public class TopicsController {
 
     @GetMapping
     public Page<TopicDto> list(@RequestParam(required = false) String courseName,
-                               @RequestParam int page,
-                               @RequestParam int quantity,
-                               @RequestParam String ordering) {
+                               @PageableDefault(sort = "id", direction = Sort.Direction.ASC, page = 0, size = 10) Pageable pagination) {
 
-        Pageable pagination = PageRequest.of(page, quantity, Sort.Direction.ASC, ordering);
         final Page<Topic> topicPage;
         if(courseName == null) {
             topicPage = topicRepository.findAll(pagination);

211f20f8272880e3507b7044e183721ee00cb939
apontes77 <alexandrepontesgyn@gmail.com>
2021-08-26 22:46:32 -0300

    habilitando spring security e acesso a endpoints get

---
+++ src/main/java/br/com/pucgo/forumecec/config/security/SecurityConfigurations.java
@@ -0,0 +1,46 @@
+package br.com.pucgo.forumecec.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    /**
+     * Configura a parte de autenticação
+     * @param auth
+     * @throws Exception
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    /**
+     * Configurações de autorização
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topics").permitAll()
+                .antMatchers(HttpMethod.GET, "/topics/*").permitAll();
+    }
+
+    /**
+     * Configurações de recursos estáticos (JS, CSS, Imagens etc)
+     * @param web
+     * @throws Exception
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
+}

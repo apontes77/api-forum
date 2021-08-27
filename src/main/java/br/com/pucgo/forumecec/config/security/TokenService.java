129b40fd6ddd69412fdd1d83562e5758d736f0e2
apontes77 <alexandrepontesgyn@gmail.com>
2021-08-26 23:50:35 -0300

    gerando tokens JWT

---
+++ src/main/java/br/com/pucgo/forumecec/config/security/TokenService.java
@@ -0,0 +1,34 @@
+package br.com.pucgo.forumecec.config.security;
+
+import br.com.pucgo.forumecec.models.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${forum.jwt.expiration}")
+    private String expiration;
+
+    @Value("${forum.jwt.secret}")
+    private String secret;
+
+    public String generateToken(Authentication authentication) {
+        final User logged = (User) authentication.getPrincipal();
+        Date today = new Date();
+        Date dateExpiration = new Date(today.getTime() + Long.parseLong(expiration));
+
+        return Jwts.builder()
+                .setIssuer("API do fórum")
+                .setSubject(logged.getId().toString())
+                .setIssuedAt(today)
+                .setExpiration(dateExpiration)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
